24e65caadbc7e4803755520ec10da4eb31162fce
Kijun Seo <kijun@dropbox.com>
2014-11-20 17:01:19 -0500

    print inventory status

---
+++ Assets/GameStatusPrinter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStatusPrinter : MonoBehaviour {
+
+    GUIText inGameConsole;
+
+	// Use this for initialization
+	void Start () {
+        inGameConsole = GetComponent<GUIText>();
+        inGameConsole.text = "";
+        NotificationCenter.defaultCenter.AddObserver(Inventory.UpdateNotificationName, HandleInventoryUpdate); 
+	}
+
+    void HandleInventoryUpdate() {
+        inGameConsole.text = Inventory.mainInventory.ToString();
+    }
+}
